9de839ae8cfc1f79212c9f9c5de301828a265d28
EdgarTrigueiros <trigueirosedgar60@gmail.com>
2025-05-08 11:41:43 +0100

    Add files via upload

---
+++ Aula06.py
@@ -0,0 +1,5 @@
+1=int(input("Coloque um algarismo: "))
+n2=int(input("Introduza outro: "))
+s=n1+n2
+# print("A soma entre ", n1 ,"+", (n2), "é" , s)
+print("A soma entre {} + {} é {}" .format(n1,n2, s))
